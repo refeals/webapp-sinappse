5c6f4b7a9d113a1c35aedd6438a785bfdcb04d13
Rafael Siqueira <rafael.siqueira@onesight.com.br>
2020-07-02 19:51:19 -0300

    Add livestream page

--- src/actions/livestream_actions.js
+++ src/actions/livestream_actions.js
@@ -1,16 +1,16 @@
 import { api } from "../api"
-import { GET_LIVESTREAM } from "../actions/action_types"
+import { GET_LIVESTREAM_ROOMS } from "../actions/action_types"
 
 import { getToken } from "../api/auth"
 
 export const getLivestream = (event_id, callback) => (dispatch, getState) => {
   api
-    .get(`/act.php?action=prog-view-livestream&event=${event_id}`, {
+    .get(`/act.php?action=live-get&event=${event_id}`, {
       headers: { Authorization: `Bearer ${getToken()}` }
     })
     .then((response) => {
       if (response.data.success)
-        dispatch({ type: GET_LIVESTREAM, payload: response.data.data })
+        dispatch({ type: GET_LIVESTREAM_ROOMS, payload: response.data.data })
       else throw Object.assign(new Error(response.data.msg), { code: 401 })
     })
     .then(() => callback && callback())
