a71746419878bb1dfaedab9306b03b9799698327
Rafael Siqueira <rafael.siqueira@onesight.com.br>
2020-07-16 20:42:24 -0300

    Fix streamer youtube player and overlay & add play/pause and mute buttons

--- src/pages/lives/index.js
+++ src/pages/lives/index.js
@@ -19,7 +19,9 @@ function Lives() {
   const streamer = useSelector((state) => state.streamer, shallowEqual)
   const dispatch = useDispatch()
 
-  const [iframeSize, setIframeSize] = useState("x1")
+  const [iframeSize, setIframeSize] = useState("400px")
+  const [playing, setPlaying] = useState(true)
+  const [volume, setVolume] = useState(1)
 
   useEffect(() => {
     document.getElementById("root").className = "desktop"
@@ -44,6 +46,9 @@ function Lives() {
 
   const fbRefStr = `event/${event.id}/livestream/${live.id}`
 
+  const togglePlaying = () => setPlaying(!playing)
+  const toggleVolume = () => setVolume(volume === 0 ? 1 : 0)
+
   return (
     <div className="talk-live-container">
       <header className="live-header">
@@ -58,26 +63,46 @@ function Lives() {
       <div className="live-content">
         <div className="content-left">
           <div className="live-video">
-            <ReactPlayer
-              url={`https://www.youtube.com/embed/${live.youtube_url}`}
-              playing={true}
-              controls={false}
-              onPlay={() => console.log("sadasdas")}
-              config={{
-                youtube: {
-                  playerVars: {
-                    autoplay: 1,
-                    controls: 0,
-                    disablekb: 1,
-                    showinfo: 0,
-                    rel: 0,
-                    iv_load_policy: 3,
-                    fs: 0,
-                    modestbranding: 1
+            <div className="live-video-container">
+              <ReactPlayer
+                url={`https://www.youtube.com/embed/${live.youtube_url}`}
+                playing={playing}
+                volume={volume}
+                controls={false}
+                config={{
+                  youtube: {
+                    playerVars: {
+                      autoplay: 1,
+                      controls: 0,
+                      disablekb: 1,
+                      showinfo: 0,
+                      rel: 0,
+                      iv_load_policy: 3,
+                      fs: 0,
+                      modestbranding: 1
+                    }
                   }
-                }
-              }}
-            />
+                }}
+                style={{
+                  zIndex: 999,
+                  maxWidth: "100%",
+                  transition: "width 0.2s ease, height 0.2s ease",
+                  margin: "0 auto 8px"
+                }}
+                width={iframeSize}
+                height={parseInt(iframeSize) / 2}
+              />
+              <div
+                className="iframe-overlay"
+                onClick={togglePlaying}
+                style={{
+                  width: iframeSize,
+                  height: parseInt(iframeSize) / 2
+                }}
+              >
+                <i className={`fas fa-${playing ? "play" : "pause"}`} />
+              </div>
+            </div>
             {/* <iframe
               className={`video ${iframeSize}`}
               frameBorder="0"
@@ -89,28 +114,34 @@ function Lives() {
             /> */}
             <div className="iframe-size-buttons">
               <button
-                className={iframeSize === "x1" ? "active" : ""}
-                onClick={() => setIframeSize("x1")}
+                className={iframeSize === "400px" ? "active" : ""}
+                onClick={() => setIframeSize("400px")}
               >
                 1x
               </button>
               <button
-                className={iframeSize === "x2" ? "active" : ""}
-                onClick={() => setIframeSize("x2")}
+                className={iframeSize === "600px" ? "active" : ""}
+                onClick={() => setIframeSize("600px")}
               >
                 2x
               </button>
               <button
-                className={iframeSize === "x3" ? "active" : ""}
-                onClick={() => setIframeSize("x3")}
+                className={iframeSize === "800px" ? "active" : ""}
+                onClick={() => setIframeSize("800px")}
               >
                 3x
               </button>
               <button
-                className={iframeSize === "x4" ? "active" : ""}
-                onClick={() => setIframeSize("x4")}
+                className={iframeSize === "1000px" ? "active" : ""}
+                onClick={() => setIframeSize("1000px")}
               >
                 4x
+              </button>
+              <button onClick={togglePlaying}>
+                <i className={`fas fa-${playing ? "play" : "pause"}`} />
+              </button>
+              <button onClick={toggleVolume}>
+                <i className={`fas fa-volume-${volume ? "up" : "mute"}`} />
               </button>
             </div>
           </div>
