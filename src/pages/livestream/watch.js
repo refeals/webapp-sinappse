fa1e8b25119ff79887eb224e7d1d66149be3c45e
Rafael Siqueira <rafael.siqueira@onesight.com.br>
2020-07-28 10:36:10 -0300

    Add padding bottom when iframe is readyu

--- src/pages/livestream/watch.js
+++ src/pages/livestream/watch.js
@@ -11,6 +11,7 @@ const Watch = ({ match }) => {
   const lives = useSelector((state) => state.lives, shallowEqual)
 
   const [playing, setPlaying] = useState(process.env.NODE_ENV === "production")
+  const [iframeReady, setIframeReady] = useState(false)
 
   const live = lives.find((l) => l.id === parseInt(match.params.live_id))
 
@@ -28,7 +29,7 @@ const Watch = ({ match }) => {
 
   return (
     <div className="live-container">
-      <div className="video-container">
+      <div className={`video-container ${iframeReady ? "iframeReady" : ""}`}>
         <ReactPlayer
           url={`https://www.youtube.com/embed/${live.youtube_url}?autoplay=1&controls=0&disablekb=1&showinfo=0&rel=0&iv_load_policy=3&fs=0&modestbranding=1`}
           className="react-player"
@@ -50,6 +51,7 @@ const Watch = ({ match }) => {
               }
             }
           }}
+          onReady={() => setIframeReady(true)}
         />
         {/* <iframe
           className="video"
