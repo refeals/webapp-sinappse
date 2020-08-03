5b5105e170c271629c01cfca34d9832b482be031
Rafael Siqueira <rafael.siqueira@onesight.com.br>
2020-08-03 15:12:25 -0300

    Register service worker

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
 import React from "react"
-import { ToastContainer } from "react-toastify"
 import Modal from "react-modal"
-
+import { ToastContainer } from "react-toastify"
 import "react-toastify/dist/ReactToastify.css"
-
 import Routes from "./routes"
 
 Modal.setAppElement("#root")
